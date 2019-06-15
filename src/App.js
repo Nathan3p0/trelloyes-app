3f1d7eadd858c14e3b03166bff2c25a6a70019be
Dora Gil <git@doragil.me>
2019-06-15 13:25:45 -0400

    Converted App to class and set up default props.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 import './App.css';
 import List from './Components/List';
 
-function App(props) {
-  const lists = props.store.lists.map((list) => {
-    const cards = list.cardIds.map(cardId => {
-      return props.store.allCards[cardId];
+class App extends React.Component {
+
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {}
+    }
+  }
+
+  render () {
+    const { store } = this.props;
+    const lists = store.lists.map((list) => {
+      const cards = list.cardIds.map(cardId => {
+        return store.allCards[cardId];
+      })
+      return <List key={list.id} header={list.header} cards={cards}/>
     })
-    return <List key={list.id} header={list.header} cards={cards}/>
-  })
 
-
-  return (
-    <main className='App'>
+    return (<main className='App'>
       <header className="App-header">
         <h1>Trelloyes!</h1>
       </header>
@@ -20,8 +28,8 @@ function App(props) {
         {lists}
       </div>
       
-    </main>
-  );
+    </main>)
+  }
 }
 
 export default App;
