3d814318c34c53dba4ff8d9e9f1c3b21bdc74d2a
Nathan Szelag <nszelag@live.com>
2019-06-15 12:07:01 -0400

    mapped over store object to display list props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import List from './Components/List';
 
-function App() {
+function App(props) {
+  const lists = props.store.lists.map((list) => {
+    return <List key={list.id} header={list.header} cards={list.cardIds}/>
+  })
+
   return (
     <main className='App'>
       <header className="App-header">
         <h1>Trelloyes!</h1>
       </header>
       <div className="App-list">
-        <List header="First List"/>
+        {lists}
       </div>
       
     </main>
