63855a5d3d36bdafadb7a66bd1a3f02474a073d8
Dora Gil <git@doragil.me>
2019-06-15 13:01:33 -0400

    FInalized list and card components

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,13 +5,12 @@ class Card extends React.Component {
     render() {
         return (
             <div className='Card'>
-                <button type='button'>Delete</button>
+                <button type='button'>delete</button>
                 <h3>{this.props.title}</h3>
                 <p>{this.props.content}</p>
             </div>
         )
     }
 }
-
 
 export default Card
--- src/Components/List.js
+++ src/Components/List.js
@@ -3,214 +3,26 @@ import './List.css'
 import Card from './Card'
 
 class List extends React.Component {
-    render() {
-      return (
+
+  render() {
+    const cards = this.props.cards.map(card => {
+      return <Card key={card.id} title={card.title} content={card.content}/>
+    })
+
+    return (
       <section className="List">
         <header className="List-header">
           <h2>{this.props.header}</h2>
         </header>
         <div className="List-cards">
-          <Card title='First Card' content='Lorem Ipsum' />
+          {cards}
           <button type="button" className="List-add-button">
               + Add Random Card
           </button>
         </div>
       </section>
-      )
-    }
+    )
   }
-
-  /* <div class="App-list"> <- <List className="App-list" />
-      <section class="List">
-        <header class="List-header">
-          <h2>First list</h2>
-        </header>
-        <div class="List-cards">
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>First card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Second card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Fifth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Sixth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Seventh card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Tenth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Twelfth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Thirteenth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-            <button type="button" class="List-add-button">
-              + Add Random Card
-            </button>
-        </div>
-      </section>
-      <section class="List">
-        <header class="List-header">
-          <h2>Second list</h2>
-        </header>
-        <div class="List-cards">
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Second card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Third card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Fourth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Sixth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Eighth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Ninth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Eleventh card</h3>
-            <p>lorem ipsum</p>
-          </div>
-            <button type="button" class="List-add-button">
-              + Add Random Card
-            </button>
-        </div>
-      </section>
-      <section class="List">
-        <header class="List-header">
-          <h2>Third list</h2>
-        </header>
-        <div class="List-cards">
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>First card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Second card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Third card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Fourth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Fifth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Sixth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Seventh card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Eighth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Ninth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Tenth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Eleventh card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Twelfth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Thirteenth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <button type="button" class="List-add-button">
-            + Add Random Card
-          </button>
-        </div>
-      </section>
-      <section class="List">
-        <header class="List-header">
-          <h2>Fourth list</h2>
-        </header>
-        <div class="List-cards">
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Twelfth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <div class="Card">
-            <button type="button">delete</button>
-            <h3>Thirteenth card</h3>
-            <p>lorem ipsum</p>
-          </div>
-          <button type="button" class="List-add-button">
-            + Add Random Card
-          </button>
-        </div>
-      </section>
-    </div> */
+}
 
 export default List;
