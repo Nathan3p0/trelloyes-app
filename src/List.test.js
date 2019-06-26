c3e088f5d99ca885ccb48662d0494e39e38278e7
Dora Gil <git@doragil.me>
2019-06-25 20:01:53 -0400

    Finilized List test

--- src/List.test.js
+++ src/List.test.js
@@ -6,11 +6,16 @@ import List from './Components/List'
 describe('List component rendering test', () => {
     it('Render component without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<List />, div);
+        ReactDOM.render(<List cards={[]} />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
     it('Renders to UI as expected', () => {
-        const tree = renderer.create(<List />).toJSON();
+        const tree = renderer.create(<List cards={[]} />).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+
+    it('Renders to UI as expected', () => {
+        const tree = renderer.create(<List cards={[{ id: 'a', title: 'First card', content: 'lorem ipsum' }]} />).toJSON();
         expect(tree).toMatchSnapshot();
     })
 })
