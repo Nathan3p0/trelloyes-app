d6e4f7e2e7ee7c46f8ed9afdbbad524138df4fa1
Dora Gil <git@doragil.me>
2019-06-25 20:56:19 -0400

    Refactored to remove store.js and use state w/Nate

--- src/App.js
+++ src/App.js
@@ -6,14 +6,49 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      
+      lists: [
+        {
+          id: '1',
+          header: 'First list',
+          cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+        },
+        {
+          id: '2',
+          header: 'Second list',
+          cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+        },
+        {
+          id: '3',
+          header: 'Third list',
+          cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+        },
+        {
+          id: '4',
+          header: 'Fourth list',
+          cardIds: [ 'l', 'm' ],
+        },
+      ],
+      allCards: {
+        'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+        'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+        'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+        'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+        'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+        'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+        'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+        'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+        'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+        'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+        'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+        'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+        'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+      }
     }
   }
   render() {
-    const { store } = this.props;
-    const lists = store.lists.map((list) => {
+    const lists = this.state.lists.map((list) => {
       const cards = list.cardIds.map(cardId => {
-        return store.allCards[cardId];
+        return this.state.allCards[cardId];
       })
       return <List key={list.id} header={list.header} cards={cards}/>
     })
@@ -31,10 +66,8 @@ class App extends Component {
   }
 
 App.defaultProps = {
-  store: {
     lists: [],
     allCards: {}
-  }
 }
 
 export default App;
